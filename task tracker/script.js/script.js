cad901a3f66eafce0ad3c84b181b8b0b9e834175
Andrew Morris <130861679+Andrewmorri-s@users.noreply.github.com>
2026-03-05 17:43:54 +0000

    Add files via upload

---
+++ task tracker/script.js/script.js
@@ -0,0 +1,17 @@
+function addTask() {
+
+let input = document.getElementById("taskInput");
+
+let li = document.createElement("li");
+
+li.textContent = input.value;
+
+li.onclick = function(){
+li.remove();
+}
+
+document.getElementById("taskList").appendChild(li);
+
+input.value = "";
+
+}
